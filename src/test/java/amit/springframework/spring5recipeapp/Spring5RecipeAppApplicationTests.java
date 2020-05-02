e81d923ae0ddfbcadf31775b797e8f27da3be7b2
amitmodi06 <amitmodi06@gmail.com>
2020-05-03 00:51:31 +0530

    Created JUnit Test for Category class. Using JUnit5

--- src/test/java/amit/springframework/spring5recipeapp/Spring5RecipeAppApplicationTests.java
+++ src/test/java/amit/springframework/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -3,6 +3,7 @@ package amit.springframework.spring5recipeapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/*@ExtendWith(SpringExtension.class)*/
 @SpringBootTest
 class Spring5RecipeAppApplicationTests {
 
