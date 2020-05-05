0c6aec701df912f9841ef49357938cc6c4be52b6
amitmodi06 <amitmodi06@gmail.com>
2020-05-05 22:57:12 +0530

    Added Spring Integration Testing in UnitOfMeasureRepositoryIT class.

---
+++ src/test/java/amit/springframework/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,33 @@
+package amit.springframework.spring5recipeapp.repositories;
+
+import amit.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Test
+    //@DirtiesContext
+    void findByDescription() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+
+    @Test
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+}
