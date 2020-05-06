1efc6619c56b053c08bf1ea49e1728ba02e35943
amitmodi06 <amitmodi06@gmail.com>
2020-05-06 08:20:54 +0530

    Using Maven Failsafe plugin for integration testing.

--- src/test/java/amit/springframework/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/amit/springframework/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -2,18 +2,16 @@ package amit.springframework.spring5recipeapp.repositories;
 
 import amit.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @DataJpaTest
-class UnitOfMeasureRepositoryIT {
+public class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
