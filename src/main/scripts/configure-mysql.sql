311b3e59417aa0333a754b75c42f54b3b1514022
KUAM <amitmodi06@gmail.com>
2020-06-13 21:14:30 +0530

    Updating configure-mysql.sql file.

--- src/main/scripts/configure-mysql.sql
+++ src/main/scripts/configure-mysql.sql
@@ -3,29 +3,29 @@
 
 # connect to mysql and run as root user
 #Create Databases
-CREATE DATABASE sfg_dev;
-CREATE DATABASE sfg_prod;
+CREATE DATABASE amit_dev;
+CREATE DATABASE amit_prod;
 
 #Create database service accounts
-CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'amit';
-CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'amit';
-CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'amit';
-CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'amit';
+CREATE USER 'amit_dev_user'@'localhost' IDENTIFIED BY 'amit';
+CREATE USER 'amit_prod_user'@'localhost' IDENTIFIED BY 'amit';
+CREATE USER 'amit_dev_user'@'%' IDENTIFIED BY 'amit';
+CREATE USER 'amit_prod_user'@'%' IDENTIFIED BY 'amit';
 
 #Database grants
-GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
-GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
-GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
-GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
-GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
-GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
-GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
-GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
-GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
-GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
-GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
-GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
-GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
-GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
-GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
-GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT SELECT ON amit_dev.* to 'amit_dev_user'@'localhost';
+GRANT INSERT ON amit_dev.* to 'amit_dev_user'@'localhost';
+GRANT DELETE ON amit_dev.* to 'amit_dev_user'@'localhost';
+GRANT UPDATE ON amit_dev.* to 'amit_dev_user'@'localhost';
+GRANT SELECT ON amit_prod.* to 'amit_prod_user'@'localhost';
+GRANT INSERT ON amit_prod.* to 'amit_prod_user'@'localhost';
+GRANT DELETE ON amit_prod.* to 'amit_prod_user'@'localhost';
+GRANT UPDATE ON amit_prod.* to 'amit_prod_user'@'localhost';
+GRANT SELECT ON amit_dev.* to 'amit_dev_user'@'%';
+GRANT INSERT ON amit_dev.* to 'amit_dev_user'@'%';
+GRANT DELETE ON amit_dev.* to 'amit_dev_user'@'%';
+GRANT UPDATE ON amit_dev.* to 'amit_dev_user'@'%';
+GRANT SELECT ON amit_prod.* to 'amit_prod_user'@'%';
+GRANT INSERT ON amit_prod.* to 'amit_prod_user'@'%';
+GRANT DELETE ON amit_prod.* to 'amit_prod_user'@'%';
+GRANT UPDATE ON amit_prod.* to 'amit_prod_user'@'%';
